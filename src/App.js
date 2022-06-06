3cf6ab71e9d5fce663358c2bdee07f58635f60e6
Brien Thomas <thomasbrien94@gmail.com>
2022-06-06 16:03:12 -0700

    Imported files to app.js, created the components for the app function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-{/* import the zodiac array */}
+import zodiac from './data.js';
+
+import Header from './Header/Header.js';
+import Main from './Main/Main.js';
+import Footer from './Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
       {/* import and use the Header. Main, and Footer components here */}
+      <Header name='Brien' />
       {/* the Header component needs a string passed in as the `name` prop */}
+      <Footer dates='2022' />
       {/* the Footer component needs a year passed in as the `year` prop */}
+      <Main zodiacs={zodiac} />
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
     </div>
   );
